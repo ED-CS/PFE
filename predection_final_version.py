a9f047cff5b43431df66a785360875c802b9abe6
unknown <djameleddine.ed@gmail.com>
2021-08-24 21:58:28 +0100

    adding a predection files and create a prediction route also template for showing a result

---
+++ predection_final_version.py
@@ -0,0 +1,157 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import librosa
+
+
+import torch
+# import torch.nn as nn
+# import torch.backends.cudnn as cudnn
+# import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from utils import  MelDataset
+from models import ResNet
+
+def convert(filename, input_dir, output_dir, SAMPLE_RATE, N_MELS, HOP_LENGTH, N_FFT, FMIN, FMAX):
+        file_path = input_dir
+        data, _ = librosa.core.load(file_path, sr=SAMPLE_RATE, res_type="kaiser_fast")
+        data = librosa.feature.melspectrogram(
+            data,
+            sr=SAMPLE_RATE,
+            n_mels=N_MELS,
+            hop_length=HOP_LENGTH, # 1sec -> 128
+            n_fft=N_FFT,
+            fmin=FMIN,
+            fmax=FMAX,
+        ).astype(np.float32)
+        np.save("{}/{}.npy".format(output_dir, filename), data)
+        return data
+
+def prediction(val_loader, model, NUM_CLASS, LOAD_DIR_MODELS, df_predict, labels_path, sysName, dic_predict):
+    sigmoid = torch.nn.Sigmoid().cuda()
+
+    # switch to eval mode
+    model.eval()
+
+    model.load_state_dict(torch.load("{}/weight_epoch_320.pth".format(LOAD_DIR_MODELS), map_location='cpu'))
+    # validate
+    preds = np.zeros([0, NUM_CLASS], np.float32)
+
+    # 
+    for i, (input, _) in enumerate(val_loader):
+        # get batches
+        input = torch.autograd.Variable(input.cpu())
+
+    # compute output
+    with torch.no_grad():
+        output = model(input)
+        pred = sigmoid(output)
+        pred = pred.data.cpu().numpy()
+
+    preds = np.concatenate([preds, pred])
+
+    df_predict[sysName] = preds[0]
+    
+    dd = pd.read_csv(labels_path)
+    dd[sysName] = preds[0]
+    dd = dd.sort_values(by=sysName, ascending=True)
+
+    systemPredection = get_prediction_dic(df=dd, columnName=sysName)
+    dic_predict.append(systemPredection)
+
+
+def load_data(path_npy, NUM_CLASS):
+    paths = [path_npy,]
+    y = [np.zeros([0, NUM_CLASS], np.float32),]
+
+    dataset_valid = MelDataset(paths, y,)
+    valid_loader = DataLoader(dataset_valid, batch_size=1,
+                            shuffle=False, num_workers=1, pin_memory=True,
+                            )
+    return valid_loader 
+    
+def get_prediction_dic(df, columnName):
+                listLabes = []
+                listPredectionValue = []
+                for i in range(79,0,-1):
+                    if df.iloc[i][columnName]*100 >= 50:
+                        listLabes.append(df.iloc[i]['Dataset'])
+                        listPredectionValue.append(df.iloc[i][columnName])
+                    else:
+                        break
+                systemPredection =	{
+                    "systemName": columnName,
+                    "tagList": listLabes,
+                    "valueList": listPredectionValue
+                }
+                return systemPredection
+
+
+def list_tags(LOAD_DIR_MODELS, output_dir, input_dir, filename, labels_path):
+
+    # parameters
+    SAMPLE_RATE = 44100
+    N_MELS = 128
+    HOP_LENGTH = 347
+    N_FFT = 128*20
+    FMIN = 20
+    FMAX = SAMPLE_RATE//2
+    NUM_CLASS = 80
+
+
+    # convert wav file to npy
+    _ = convert(filename=filename, input_dir=input_dir, output_dir=output_dir, 
+                SAMPLE_RATE=SAMPLE_RATE, N_MELS=N_MELS, HOP_LENGTH=HOP_LENGTH, 
+                N_FFT=N_FFT, FMIN=FMIN, FMAX=FMAX)
+
+    # build model
+    model = ResNet(NUM_CLASS) # in case where we using cPU
+
+    # get labels for predect dataframe
+    df_predict = pd.read_csv(labels_path)
+    valid_loader = load_data(path_npy='{}/{}.npy'.format(output_dir, filename), NUM_CLASS=NUM_CLASS)
+
+    # prediction processe 
+    dic_predict = []
+    
+    for pat in range(5): 
+        if pat <=2 : 
+            sysName = 'System {}'.format(pat)
+            prediction(val_loader=valid_loader, model=model, NUM_CLASS=NUM_CLASS,
+                        LOAD_DIR_MODELS=LOAD_DIR_MODELS[pat], df_predict=df_predict, labels_path=labels_path, 
+                        sysName=sysName, dic_predict=dic_predict)
+        
+        else:
+            sysName = 'System {}'.format(pat)
+            prediction(val_loader=valid_loader, model=model, NUM_CLASS=NUM_CLASS,
+                        LOAD_DIR_MODELS=LOAD_DIR_MODELS[pat], df_predict=df_predict, labels_path=labels_path, 
+                        sysName=sysName, dic_predict=dic_predict)
+      
+    return dic_predict, df_predict
+
+#-------------------------------------------------------------------------------------------------------------- 
+
+#--------------------------------------------------------------------------------------------------------------
+def main():
+
+    LOAD_DIR_MODELS = ['C:/Users/mahrati_ed/Desktop/route predection backend/models/system_0',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_1',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_2mean',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_3',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_4']
+   
+    output_dir = 'C:/Users/mahrati_ed/Desktop/route predection backend/out'
+    input_dir ='D:/pfe dataset/test/4c9d698d.wav'
+    filename= '4c9d698d'
+    labels_path = 'C:/Users/mahrati_ed/Desktop/route predection backend/labels.csv'
+
+    dic_predict, df_predict = list_tags(LOAD_DIR_MODELS=LOAD_DIR_MODELS, output_dir=output_dir, 
+                                        input_dir=input_dir, filename=filename, labels_path=labels_path)
+
+    print(dic_predict)
+    print(df_predict)
+
+if __name__ == '__main__':
+    main()
--- run.py
+++ run.py
@@ -3,48 +3,71 @@ from flask import Flask, render_template, url_for, redirect, session, flash, req
 from forms import GetTagsForm, LoginForm, RegistrationForm
 from werkzeug.utils import secure_filename
 
+from predection_final_version import list_tags
+
+
 app = Flask(__name__)
 
-UPLOAD_PATH="uploaded file/"
+WAV_PATH="wav_file/"
+NPY_PATH = "npy_file/"
+LABELS_PATH = 'C:/Users/mahrati_ed/Desktop/pfe_app/labels.csv'
+LOAD_DIR_MODELS = ['C:/Users/mahrati_ed/Desktop/route predection backend/models/system_0',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_1',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_2mean',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_3',
+                'C:/Users/mahrati_ed/Desktop/route predection backend/models/system_4']
+
 
 app.config['SECRET_KEY']='6ea28f4cb420aa71a93faf8acf4c37bd'
-app.config['UPLOAD_PATH'] = UPLOAD_PATH
+app.config['WAV_PATH'] = WAV_PATH
+app.config['NPY_PATH'] = NPY_PATH
+app.config['LABELS_PATH'] = LABELS_PATH
+app.config['LOAD_DIR_MODELS'] = LOAD_DIR_MODELS
+
+
+
 
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
+    
     form = GetTagsForm()
     if request.method == 'POST':
-        uploaded_file = request.files['audio']
-        filename= secure_filename(uploaded_file.filename)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
-        session['filename'] = filename
-        session['file_path'] = file_path
+        wav_file = request.files['audio']
+        wavefile_name = SaveWavFile(wav_file)
+        session['wavefile_name'] = wavefile_name
 
-        return redirect(url_for('quick_test_result', filename=filename))
+        return redirect(url_for('quick_test_result'))
         
     return render_template('index.html', title='Home', form=form)
 
-
-'''
-@app.route("/get_ClipPath", methods=['GET', 'POST'])
-def quick_test():
-    form = GetTagsForm()
-    if form.validate_on_submit():
-        clip_path = form.clip.data
-        session['clip_path'] = clip_path
-        return redirect(url_for('quick_test_result', title='Quick Test Result'))
-
-    return render_template('index.html', title='Get Audio Path', form=form)
-'''
-
 @app.route("/quick_test_result" )
 def quick_test_result():
-    # audio processing and get tags in this route 
-    file_path = session.get('file_path', None)
-    return render_template('quick_test_result.html', title='Quick Test Result', file_path=file_path)
+
+    wavefile_name = session.get('wavefile_name', None)
+    WavFile_path, NpyFile_path, filename = getFilePaths(wavefile_name=wavefile_name)
+    labels_path = app.config['LABELS_PATH']
+    LOAD_DIR_MODELS = app.config['LOAD_DIR_MODELS']
+
+    # audio processing and get tags
+    dic_predict, df_predict = list_tags(LOAD_DIR_MODELS=LOAD_DIR_MODELS, output_dir=NpyFile_path, 
+                                        input_dir=WavFile_path, filename=filename, labels_path=labels_path)
+    print(dic_predict)
+    print(df_predict)
+      
+
+
+
+
+
+    # delete  wav and npy files
+    # _ = DeleteFile(file_path=WavFile_path)
+    # _ = DeleteFile(file_path=NpyFile_path)
+    # get tags in this route 
+
+    
+    return render_template('quick_test_result.html', title='Quick Test Result', filename=wavefile_name , dic_predict=dic_predict)
 
 @app.route("/get_detail_result", methods=['GET', 'POST'])
 def get_detail_result():
@@ -65,6 +88,35 @@ def register():
 
 
 
+def DeleteFile(path):
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        print("The file does not exist") 
+
+def SaveWavFile(wav_file):
+    wavefile_name= secure_filename(wav_file.filename)
+    wav_file.save(os.path.join(app.config['WAV_PATH'], wavefile_name))
+    return wavefile_name
+
+def getFilePaths(wavefile_name):
+    wavfile_path = os.path.join(app.config['WAV_PATH'], wavefile_name)
+    filename = wavefile_name[:-4]
+    NpyFile_path = app.config['NPY_PATH']
+    return wavfile_path, NpyFile_path, filename
+
+
+'''
+@app.route("/get_ClipPath", methods=['GET', 'POST'])
+def quick_test():
+    form = GetTagsForm()
+    if form.validate_on_submit():
+        clip_path = form.clip.data
+        session['clip_path'] = clip_path
+        return redirect(url_for('quick_test_result', title='Quick Test Result'))
+
+    return render_template('index.html', title='Get Audio Path', form=form)
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
